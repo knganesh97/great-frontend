ec159ae338bd247532f1c572937f9fda7349569d
Ganesh Kantimahanthi <60934037+knganesh97@users.noreply.github.com>
2025-11-30 23:33:57 +0530

    feat: update padding on ProfileCardPage for improved layout

--- src/app/profile-card/page.tsx
+++ src/app/profile-card/page.tsx
@@ -3,7 +3,7 @@ import ProfileCard from "@/components/ProfileCard";
 export default function ProfileCardPage() {
     return (
         <div 
-            className="flex flex-col items-center py-[120px] px-0 relative min-h-screen mx-auto" 
+            className="flex flex-col items-center py-[200px] px-0 relative min-h-screen mx-auto" 
             style={{ background: 'linear-gradient(147.52deg, #F9FAFB 8.89%, #D2D6DB 100.48%)' }}
         >
             <ProfileCard/>
